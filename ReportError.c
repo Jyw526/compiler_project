b086d62ceb5e25cf58270d7aa27d5d386be97827
NIGHTLINKER <58656624+NIGHTLINKER@users.noreply.github.com>
2020-05-11 00:00:12 +0900

    edit glob.h & symtable.c error fixed

--- ReportError.c
+++ ReportError.c
@@ -1,8 +1,13 @@
+/* ReportError.c - 각 error에 대한 출력
+ * programmer - 백지수,송주은,정연우
+ * date - 05/04/2020
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "tn.h"
 #include "glob.h"
-extern cErrors;
+
 void reportError(ERRORtypes err) {
 	switch (err) {
 	case ILLICHAR :
