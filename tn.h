c9bf6721f82c8298587e5e23ac9a65f06babba95
NIGHTLINKER <58656624+NIGHTLINKER@users.noreply.github.com>
2020-06-04 00:06:28 +0900

    Add files via upload

--- tn.h
+++ tn.h
@@ -1,9 +1,48 @@
-#pragma once
-/* tn.h - token type
- * progrmmer 백지수, 송주은, 정연우
- * date - 05/04/2020
- */
-enum token { TEOF, TCONST, TELSE, TIF, TINT, TRETURN, TVOID, TWHILE, TPLUS, TMINUS, TSTAR, TSLASH, TMOD,
-			 TASSIGN, TADDASSIGN, TSUBASSIGN, TMULASSIGN, TDIVASSIGN, TMODASSIGN, TNOT, TAND, TOR,
-			 TEQUAL, TNOTEQU, TLESS, TGREAT, TLESSE, TGREATE, TINC, TDEC, TIDENT, TNUMBER, TRNUMBER, TCOMMA, TSEMI,
-			 TBROPEN, TBRCLOSE, TCURLOPEN, TCURLCLOSE, TSQUOPEN, TSQUCLOSE, TILLICHAR, TILLIDENT };
+#ifndef YYSTYPE
+#define YYSTYPE int
+#endif
+#define	TIDENT	258
+#define	TNUMBER	259
+#define	TCONST	260
+#define	TELSE	261
+#define	TIF	262
+#define	TINT	263
+#define	TRETURN	264
+#define	TVOID	265
+#define	TWHILE	266
+#define	TRNUMBER	267
+#define	TFLOAT	268
+#define	TADDASSIGN	269
+#define	TSUBASSIGN	270
+#define	TMULASSIGN	271
+#define	TDIVASSIGN	272
+#define	TMODASSIGN	273
+#define	TOR	274
+#define	TAND	275
+#define	TEQUAL	276
+#define	TNOTEQU	277
+#define	TGREAT	278
+#define	TLESS	279
+#define	TGREATE	280
+#define	TLESSE	281
+#define	TINC	282
+#define	TDEC	283
+#define	TPLUS	284
+#define	TMINUS	285
+#define	TSTAR	286
+#define	TSLASH	287
+#define	TMOD	288
+#define	TASSIGN	289
+#define	TNOT	290
+#define	TCOMMA	291
+#define	TSEMI	292
+#define	TBROPEN	293
+#define	TBRCLOSE	294
+#define	TCURLOPEN	295
+#define	TCURLCLOSE	296
+#define	TSQUOPEN	297
+#define	TSQUCLOSE	298
+#define	LOWER_THAN_ELSE	299
+
+
+extern YYSTYPE yylval;
